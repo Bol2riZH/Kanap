627e55e23a84fc6e571e44dc65575c9aec19cdf2
Matthieu Reungoat <reungoat.matthieu@gmail.com>
2022-07-25 15:43:55 +0200

    Get data from API - Show products to index.html

---
+++ front/js/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+// INIT CONST
+const productsContainer = document.querySelector('.items');
+
+// GET DATA FROM THE API
+const getProductsData = async function () {
+  try {
+    const result = await fetch(`http://localhost:3000/api/products`);
+    if (!result.ok) throw new Error('Problem with API');
+
+    const data = await result.json();
+    data.forEach(element => showProducts(element));
+  } catch (error) {
+    console.log(error);
+  }
+};
+getProductsData();
+
+// INSERT THE PRODUCTS TO THE HTML
+const showProducts = function (data) {
+  const html = `
+    <a href="./product.html?id=42">
+    <article>
+        <img src="${data.imageUrl}" alt="${data.altTxt}">
+        <h3 class="productName">${data.name}</h3>
+        <p class="${data.description}</p>
+    </article>
+    </a>
+    `;
+  productsContainer.insertAdjacentHTML('beforeend', html);
+};
